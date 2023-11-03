8ea3006d6ef409dff020d60bb61b8429b863759f
Cameron Fleming <cameron@nevexo.space>
2023-11-03 14:30:55 +0000

    routers: add Authentication and UserManagement Routers

---
+++ server/routes/Authentication.js
@@ -0,0 +1,76 @@
+// voip-phonebook - Authentication Endopints
+// (c) Cameron Fleming 2023.
+
+import { Router } from "express";
+import { nanoid } from 'nanoid';
+
+import {
+  get_user_by_email,
+  verify_user_password
+} from "../auth/Users.js";
+
+import {
+  create_user_session,
+  delete_session,
+  get_all_user_sessions_safe,
+  prune_user_sessions
+} from "../auth/Sessions.js";
+
+import { logger } from "../index";
+import { get_and_validate_session } from "../middleware/Authentication.js";
+
+export const router = Router();
+
+router.post("/session", async (req, res) => {
+  // Login a user.
+  // Check if the user exists.
+  // Check for required body elements
+  if (!req.body.email_address) {
+    return res.status(400).json({ error: "missing_email_address" })
+  } else if (!req.body.password) {
+    return res.status(400).json({ error: "missing_password" })
+  }
+
+  const user = await get_user_by_email(req.body.email_address)
+  if (!user) {
+    return res.status(401).json({ error: "user_does_not_exist" })
+  }
+
+  // Check if the password is correct.
+  if (!await verify_user_password(user.id, req.body.password)) {
+    return res.status(401).json({ error: "incorrect_password" })
+  }
+
+  // Create a new session.
+  const session = await create_user_session(user.id, req.headers['user-agent'])
+
+  // Return the session.
+  return res.status(200).json(session)
+})
+
+router.get("/session", get_and_validate_session, async (req, res) => {
+  // Validate a session using the session middleware.
+
+  // // Return the session.
+  return res.status(200).json({ user: req.user, session: req.session })
+})
+
+router.get("/sessions", get_and_validate_session, async (req, res) => {
+  // Get all sessions for this user
+
+  return res.status(200).json(get_all_user_sessions_safe(req.user.id));
+})
+
+router.delete("/session", get_and_validate_session, async (req, res) => {
+  // Delete the session.
+  await delete_session(req.session.id)
+
+  return res.status(200).json({ status: "session_deleted" })
+})
+
+router.delete("/sessions", get_and_validate_session, async (req, res) => {
+  // Delete all sessions for a user.
+  await prune_user_sessions(req.user.id)
+
+  return res.status(200).json({ status: "all_sessions_deleted" })
+})
---
+++ server/routes/UserManagement.js
@@ -0,0 +1,152 @@
+// voip-phonebook - User management/authentication endpoints.
+// Endpoints used to create, manage, and delete users.
+// (c) Cameron Fleming 2023.
+
+import { Router } from "express";
+import { nanoid } from 'nanoid';
+import { User } from "../types/User.js";
+import { UserSession } from "../types/UserSession";
+import { 
+  create_user, 
+  get_user_safe, 
+  delete_user, 
+  change_user_password, 
+  change_user_name, 
+  change_user_remark,
+  change_user_root_status,
+  get_all_users
+} from "../auth/Users.js";
+import {
+  prune_user_sessions
+} from "../auth/Sessions.js";
+import { get_and_validate_session, is_root } from "../middleware/Authentication.js";
+import { logger } from "../index";
+
+export const router = Router();
+
+router.post("/", get_and_validate_session, is_root, async (req, res) => {
+  // Create a new user
+  // Check for required body elements
+  if (!req.body.name) {
+    return res.status(400).json({ error: "missing_name" })
+  } else if (!req.body.email_address) {
+    return res.status(400).json({ error: "missing_email_address" })
+  } else if (!req.body.password) {
+    return res.status(400).json({ error: "missing_password" })
+  }
+
+  // Require password to have a minimum length of 8 characters.
+  if (req.body.password.length < 8) {
+    return res.status(400).json({ error: "password_complexity_policy_not_met" })
+  }
+
+  // Regex email address
+  const email_regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  if (!email_regex.test(req.body.email_address)) {
+    return res.status(400).json({ error: "invalid_email_address" })
+  }  
+
+  // Create the user.
+  const user = await create_user(
+    req.body.name,
+    req.body.email_address,
+    req.body.password,
+    false, // Root user cannot be created this way, must be upgraded later.
+    req.body.remark || "N/A"
+  )
+
+  if (user.error) {
+    return res.status(400).json({ error: user.error })
+  }
+
+  // Return the user.
+  return res.status(200).json(await get_user_safe(user.id))
+})
+
+router.get("/", get_and_validate_session, is_root, async (req, res) => {
+  // Get all users
+  return res.status(200).json(await get_all_users())
+})
+
+router.get("/:id", get_and_validate_session, async (req, res) => {
+  // Get a user by ID
+  if (!req.user.root_user && req.user.id != req.params.id) {
+    return res.status(403).json({ 
+      error: "user_is_not_root_or_self", 
+      message: "You are not a root user, and the user ID you're fetching is not yourself."
+    })
+  }
+
+  const user = await get_user_safe(req.params.id)
+  if (!user) {
+    return res.status(404).json({ error: "user_does_not_exist" })
+  }
+
+  return res.status(200).json(user)
+})
+
+router.post("/:id/root", get_and_validate_session, is_root, async (req, res) => {
+  // Upgrade a user to root
+  const user = await get_user_safe(req.params.id)
+  if (!user) {
+    return res.status(404).json({ error: "user_does_not_exist" })
+  }
+
+  // Upgrade the user to root.
+  await change_user_root_status(req.params.id, true)
+
+  return res.status(200).json({ success: true })
+})
+
+router.delete("/:id/root", get_and_validate_session, is_root, async (req, res) => {
+  // Downgrade a user from root
+  const user = await get_user_safe(req.params.id)
+  if (!user) {
+    return res.status(404).json({ error: "user_does_not_exist" })
+  }
+
+  // Downgrade the user from root.
+  await change_user_root_status(req.params.id, false)
+
+  return res.status(200).json({ success: true })
+});
+
+router.patch("/:id/password", get_and_validate_session, async (req, res) => {
+  if (!req.user.root_user && req.user.id != req.params.id) {
+    return res.status(403).json({ 
+      error: "user_is_not_root_or_self", 
+      message: "You are not a root user, and the user ID you're trying to change the password of is not yourself."
+    })
+  }
+
+  // Change a user's password
+  if (!req.body.password) {
+    return res.status(400).json({ error: "missing_password" })
+  }
+
+  // Require password to have a minimum length of 8 characters.
+  if (req.body.password.length < 8) {
+    return res.status(400).json({ error: "password_complexity_policy_not_met" })
+  }
+
+  // Change the user's password.
+  await change_user_password(req.params.id, req.body.password)
+
+  return res.status(200).json({ success: true })
+})
+
+router.delete("/:id", get_and_validate_session, is_root, async (req, res) => {
+  // Delete a user by ID
+  const user = await get_user_safe(req.params.id)
+  if (!user) {
+    return res.status(404).json({ error: "user_does_not_exist" })
+  }
+
+  // Invalidate all sessions
+  await prune_user_sessions(req.params.id)
+
+  await delete_user(req.params.id)
+
+  return res.status(200).json({ success: true })
+})
+

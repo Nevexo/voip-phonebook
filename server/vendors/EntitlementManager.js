b5c327683330acb72cc702847fb1acca96e55a0f
Cameron Fleming <cameron@nevexo.space>
2024-02-02 11:41:57 +0000

    vendors: entitlements: add checking for valid route mapping

--- server/vendors/EntitlementManager.js
+++ server/vendors/EntitlementManager.js
@@ -6,6 +6,7 @@ import { logger } from "../index";
 
 import { get_site } from "../site/SiteManage";
 import { get_service_by_id } from "./VendorManager";
+import { get_phonebook_fields_for_site } from "../fields/PhonebookFieldManager";
 import { VendorServiceEntitlement } from "../types/VendorServiceEntitlement";
 
 export const get_entitlements = async () => {
@@ -23,6 +24,13 @@ export const get_entitlement_for_vendor = async (vendor_service_id) => {
   return await VendorServiceEntitlement.find({ vendor_service: vendor_service });
 }
 
+export const get_entitlements_for_site = async (site_id) => {
+  const site = await get_site(site_id);
+  if (!site) return {error: "site_does_not_exist"};
+
+  return await VendorServiceEntitlement.find({ site: site }).populate("vendor_service").populate("site");
+}
+
 export const create_entitlement = async (site_id, vendor_service_id, configuration, field_mapping) => {
   // Check a service with this name doesn't already exist.
   const vendor_service = await get_service_by_id(vendor_service_id);
@@ -31,12 +39,34 @@ export const create_entitlement = async (site_id, vendor_service_id, configurati
   const site = await get_site(site_id);
   if (!site) return { error: "site_does_not_exist" };
 
+  const site_fields = await get_phonebook_fields_for_site(site_id);
+
   const existing_entitlement = await VendorServiceEntitlement.findOne({ site: site, vendor_service: vendor_service });
   if (existing_entitlement) return { error: "entitlement_already_exists" };
 
+  // Field mapping is expected to be:
+  // { "site_field": "vendor_field" }
+
+  // Ensure field mapping is correct
+  // Check all site requested fields are present in the field_mapping
+  for (const field of site_fields) {
+    if (!field.required) continue;
+    if (!field_mapping[field.id]) return { error: "required_field_missing", message: `Field ${field.name} is required for this site.` };
+  }
+
+  // Check all specified mappings are valid against this site
+  for (const field in field_mapping) {
+    if (!site_fields.some(f => f.id === field)) return { error: "invalid_field_mapping", message: `Field ${field} is not valid for this site.` };
+  }
+
+  // Check all specified mappings are valid against this vendor service.
+  for (const field in field_mapping) {
+    if (!vendor_service.supported_fields.some(f => f.name === field_mapping[field])) return { error: "invalid_field_mapping", message: `Field ${field_mapping[field]} is not valid for this vendor service.` };
+  }
+
   const entitlement = new VendorServiceEntitlement({
     site: site,
-    vendor_service: vendor_service_id,
+    vendor_service: vendor_service,
     configuration: configuration,
     field_mapping: field_mapping,
   });
